efc8f256be35f6336dffb0b2fb1951e084576e6b
Syn Zhang <zeyanzhang.china@gmail.com>
2022-09-12 17:02:17 +0800

    docs(ssr): add documentation to error boundary

--- templates/server-side-rendering/components/ErrorBoundary/index.tsx
+++ templates/server-side-rendering/components/ErrorBoundary/index.tsx
@@ -1,3 +1,20 @@
+/**
+ * Error Boundaries
+ *
+ * Error boundaries catch errors during rendering, in lifecycle methods,
+ * and in constructors of the whole tree below them.
+ *
+ * Errors happen outside of the React rendering lifecycle will not be
+ * caught by error boundary:
+ *
+ * Event handlers
+ * Asynchronous code (e.g. setTimeout or requestAnimationFrame callbacks)
+ * Server side rendering
+ * Errors thrown in the error boundary itself (rather than its children)
+ *
+ * @reference https://github.com/bvaughn/react-error-boundary
+ */
+
 import { Component } from 'react';
 
 interface Props {
@@ -16,12 +33,14 @@ class ErrorBoundary extends Component {
   }
 
   static getDerivedStateFromError (error) {
-    // 更新 state 使下一次渲染能够显示降级后的 UI
+    // Update state so the next render will show the fallback UI
     return { hasError: true };
   }
 
   componentDidCatch (error, errorInfo) {
-    console.error(error, errorInfo)
+    // @TODO
+    // Log the error to an error reporting service
+    console.error(error, errorInfo);
   }
 
   refresh () {
@@ -29,18 +48,18 @@ class ErrorBoundary extends Component {
   }
 
   render() {
-    if (this.state.hasError) {
-      return (
-        <div>
-          <div>Error</div>
-          <button onClick={this.refresh}>
-            Refresh
-          </button>
-        </div>
-      );
+    if (!this.state.hasError) {
+      return this.props.children;
     }
 
-    return this.props.children;
+    return (
+      <div>
+        <div>Error</div>
+        <button onClick={this.refresh}>
+          Refresh
+        </button>
+      </div>
+    );
   }
 }
 
