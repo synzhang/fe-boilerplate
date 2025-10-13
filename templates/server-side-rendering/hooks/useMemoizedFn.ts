c6e3277341f117ae571c12227c8791ee6ccf7e43
Syn Zhang <zeyanzhang.china@gmail.com>
2025-10-13 17:49:59 +0800

    feat: create useMemoizedFn react hook

---
+++ templates/server-side-rendering/hooks/useMemoizedFn.ts
@@ -0,0 +1,28 @@
+import { useMemo, useRef } from 'react';
+
+type noop = (this: any, ...args: any[]) => any;
+
+type PickFunction<T extends noop> = (
+  this: ThisParameterType<T>,
+  ...args: Parameters<T>
+) => ReturnType<T>;
+
+const useMemoizedFn = <T extends noop>(fn: T) => {
+  const fnRef = useRef<T>(fn);
+
+  // why not write `fnRef.current = fn`?
+  // https://github.com/alibaba/hooks/issues/728
+  fnRef.current = useMemo<T>(() => fn, [fn]);
+
+  const memoizedFn = useRef<PickFunction<T>>(undefined);
+
+  if (!memoizedFn.current) {
+    memoizedFn.current = function (this, ...args) {
+      return fnRef.current.apply(this, args);
+    };
+  }
+
+  return memoizedFn.current;
+};
+
+export default useMemoizedFn;
